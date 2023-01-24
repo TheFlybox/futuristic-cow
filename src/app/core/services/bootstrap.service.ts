e534fd72abf4d66e6716c2d24d1d846f1a6a04f4
Isocrates Flybox <teamflybox@gmail.com>
2023-01-23 23:39:33 -0400

    feat: Adding dynamic settings

--- src/app/core/services/bootstrap.service.ts
+++ src/app/core/services/bootstrap.service.ts
@@ -1,15 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { of, Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { Environment } from 'src/global';
+import { ConfigUrls } from '../enums/api-urls.enum';
 import { ConfigCatService } from './config-cat.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BootstrapService {
-  constructor(private configCatService: ConfigCatService) {}
+  constructor(
+    private httpClient: HttpClient,
+    private configCatService: ConfigCatService,
+  ) {}
 
-  initialize(): Observable<unknown> {
-    return of(this.configCatService.newClient(environment.configCatKey));
+  initialize(): Observable<Environment> {
+    return new Observable<Environment>(observer => {
+      this.httpClient.get<Environment>(ConfigUrls.DEFAULT).subscribe(result => {
+        this.configCatService.newClient(result.configCatKey);
+        observer.next(result);
+        observer.complete();
+      });
+    });
   }
 }
--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-import { Environment } from 'src/global';
-
-export const environment: Environment = {
-  production: true,
-  configCatKey: process.env.CONFIG_CAT_KEY_PRODUCTION,
-};
