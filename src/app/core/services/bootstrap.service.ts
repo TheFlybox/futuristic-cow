99c7f96e1b4db58fe061586e292f8362ab7afe3e
Isocrates Flybox <teamflybox@gmail.com>
2023-01-30 01:44:42 -0400

    Some code cleaning

--- src/app/core/services/bootstrap.service.ts
+++ src/app/core/services/bootstrap.service.ts
@@ -1,26 +1,13 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Environment } from 'src/global';
-import { ConfigUrls } from '../enums/api-urls.enum';
-import { ConfigCatService } from './config-cat.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BootstrapService {
-  constructor(
-    private httpClient: HttpClient,
-    private configCatService: ConfigCatService,
-  ) {}
-
-  initialize(): Observable<Environment> {
-    return new Observable<Environment>(observer => {
-      this.httpClient.get<Environment>(ConfigUrls.DEFAULT).subscribe(result => {
-        this.configCatService.newClient(result.configCatKey);
-        observer.next(result);
-        observer.complete();
-      });
+  initialize(): Observable<number> {
+    return new Observable<number>(observer => {
+      observer.next(1);
     });
   }
 }
